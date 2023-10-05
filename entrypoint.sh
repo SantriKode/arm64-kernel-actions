adcfe175227176a0256f2a62f9f74b757ebaacc2
Ahmad Nauval Syahputra <103440662+SantriKode@users.noreply.github.com>
2023-10-05 17:03:13 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,7 +38,7 @@ apt update && apt upgrade -y
 msg "Installing essential packages..."
 apt install -y --no-install-recommends git make bc bison openssl \
     curl zstd tar libxml2 zip kmod cpio flex libelf-dev libssl-dev libtfm-dev wget \
-    device-tree-compiler ca-certificates python3 python2 xz-utils
+    device-tree-compiler ca-certificates python3 xz-utils
 ln -sf "/usr/bin/python${python_version}" /usr/bin/python
 set_output hash "$(cd "$kernel_path" && git rev-parse HEAD || exit 127)"
 msg "Installing toolchain..."
